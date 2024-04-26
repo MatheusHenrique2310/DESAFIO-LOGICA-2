07d43180047d2331e1268cde7c66b569a073e39b
MatheusHenrique2310 <165209750+MatheusHenrique2310@users.noreply.github.com>
2024-04-26 13:22:34 -0300

    desafio do felipao 2

---
+++ indexx.js
@@ -0,0 +1,25 @@
+let resultado = vitoriasDerrotas (90,10)
+let niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendario"]
+
+function vitoriasDerrotas (vitoria, derrota){
+
+let subtracao = vitoria - derrota
+
+return subtracao
+
+
+
+}switch(resultado){
+case 60:
+console.log("O Herói tem de saldo de " + resultado + " está no nível de " + niveis[1])
+
+
+case 70:
+console.log("O Herói tem de saldo de " + resultado + " está no nível de " + niveis[2])
+
+
+case 80:
+console.log("O Herói tem de saldo de " + resultado + " está no nível de " + niveis[3])
+
+
+}
